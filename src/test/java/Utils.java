af194b262bbe5a44967547a63bbe89c45ea56ba5
MiloJWoj <110421152+MiloJWoj@users.noreply.github.com>
2022-08-01 21:37:29 +0200

    Update Utils.java

--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -1,4 +1,5 @@
 public class Utils {
     final static String BASE_URL = "https://bankier.pl";
+    final static String SECOND_URL= "http://api.nbp.pl/api/cenyzlota/today";
     final static String CHROME_DRIVER_LOCATION = "chromediver";
 }
