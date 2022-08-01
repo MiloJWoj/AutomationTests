72197c5137398f125c7591d01b35320c21087c7b
MiloJWoj <110421152+MiloJWoj@users.noreply.github.com>
2022-08-01 21:02:54 +0200

    Add files via upload

---
+++ src/test/java/TestPlan.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations. BeforeSuite;
+import org.testng.annotations.Test;
+
+public class TestPlan {
+
+    private static final WebDriver driver = new ChromeDriver();
+
+    @BeforeSuite
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", Utils.CHROME_DRIVER_LOCATION);
+    }
+
+        @Test(testName = "Submit a WebForm")
+        public static void submitForm () {
+            driver.manage().window().maximize();
+            driver.get(Utils.BASE_URL);
+            WebForm webForm = new WebForm(driver);
+            driver.findElement(By.xpath("//*[text()='Akceptuję']")).click();
+            Actions action = new Actions(driver);
+
+            JavascriptExecutor executor = (JavascriptExecutor) driver;
+            WebElement hoverElement =  driver.findElement(By.xpath("(//span[contains(text(), 'Rynki')])[1]"));
+            executor.executeScript("arguments[0].scrollIntoView(true);", hoverElement);
+            hoverElement.click();
+
+            driver.findElement(By.xpath("//*[text()='Surowce']")).click();
+            boolean eleSelected= driver.findElement(By.xpath("/html/body/div[3]/div[1]/div[2]/div[2]/div[1]")).isDisplayed();
+            System.out.println("Element displayed is :"+Display);
+        }
+
+        @AfterSuite
+
+        public static void cleanup () {
+
+            driver.manage().deleteAllCookies();
+            driver.close();
+        }
+    }
---
+++ src/test/java/Utils.java
@@ -0,0 +1,4 @@
+public class Utils {
+    final static String BASE_URL = "https://bankier.pl";
+    final static String CHROME_DRIVER_LOCATION = "chromediver";
+}
